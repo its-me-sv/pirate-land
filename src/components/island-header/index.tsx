1f022d37f7220d5902e3bcc35a2bb983aea9c68b
Suraj Vijay <surajcpp115@gmail.com>
2022-02-24 21:40:34 +0530

    compentized island header

---
+++ src/components/island-header/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 0rem 0.6rem;
+`;
+
+const HeaderText = styled.span`
+  font-family: calibri;
+  font-size: 3.6vh;
+  opacity: 0.7;
+`;
+
+const TitleText = styled.span`
+  font-family: "PirateKids";
+  font-size: 6.3vh;
+  color: #e61d30;
+  -webkit-text-stroke-width: 2.1px;
+  -webkit-text-stroke-color: #354b7d;
+`;
+
+interface IslandHeaderProps {
+    gameId: string|undefined;
+}
+
+const IslandHeader: React.FC<IslandHeaderProps> = ({gameId}) => {
+    return (
+      <Header>
+        <HeaderText>{gameId}</HeaderText>
+        <TitleText>Pirate Land</TitleText>
+        <HeaderText>Round - 1</HeaderText>
+      </Header>
+    );
+};
+
+export default IslandHeader;
--- src/pages/island.page.tsx
+++ src/pages/island.page.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 
+import styled from 'styled-components';
+
 import ChatContainer from '../components/chat-container';
+import IslandHeader from '../components/island-header';
+
+const IslandContainer = styled.div``;
+
+const GameArea = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  gap: 0.3rem;
+  padding: 0 0.3rem;
+  height: 90vh;
+`;
 
 interface IslandPageProps {}
 
 const IslandPage: React.FC<IslandPageProps> = () => {
     const {gameId} = useParams();
     return (
-      <div className="island-container">
-        <div className="header">
-          <span className="header-txt">{gameId}</span>
-          <span className="title-txt">Pirate Land</span>
-          <span className="header-txt">Round - 1</span>
-        </div>
-        <div className="game-area">
+      <IslandContainer>
+        <IslandHeader gameId={gameId} />
+        <GameArea>
           <ChatContainer title="Team Chat" variant={1} />
           <div className="board">Game Board</div>
           <ChatContainer title="World Chat" variant={2} />
-        </div>
-      </div>
+        </GameArea>
+      </IslandContainer>
     );
 };
 
