64b891dfd3c720a127070d5f3fff13a4e0a0740f
Suraj Vijay <surajcpp115@gmail.com>
2022-03-06 23:11:12 +0530

    handling user logout

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 import {Title} from '../home/styles';
 import Button from '../../components/button';
 import RoomsForm from '../../components/rooms-form';
 import UpdateForm from '../../components/update-form';
 import GamesRecord from "../../components/games-record";
+import BlockLoader from '../../components/block-loader';
+
+import {useAPIContext} from '../../contexts/api.context';
+import {useUserContext} from '../../contexts/user.context';
 
 import {
   ProfileContainer,
@@ -15,12 +20,27 @@ import {
 interface ProfilePageProps {}
 
 const ProfilePage: React.FC<ProfilePageProps> = () => {
+    const {REST_API} = useAPIContext();
+    const {token, setId, setToken, setLoading, loading} = useUserContext();
+    
     const logoutUser = () => {
-        window.alert("user logout");
+      setLoading!(true);
+      axios.delete(`${REST_API}/auth/logout`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        setId!('');
+        setToken!('');
+        setLoading!(false);
+      })
+      .catch(() => setLoading!(false));
     };
 
     return (
       <ProfileContainer>
+        {loading && <BlockLoader />}
         <Title>Pirate Land</Title>
         <ContentContainer>
           <RoomsForm />
