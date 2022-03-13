6aef2e75eed15f3696c50c4e4209abe62766514a
Suraj Vijay <surajcpp115@gmail.com>
2022-03-13 21:54:10 +0530

    partial fix

--- src/components/game-grid/index.tsx
+++ src/components/game-grid/index.tsx
@@ -23,23 +23,18 @@ const GameGrid: React.FC<GameGridProps> = () => {
     const onBoxClick = async (player: number, typ: number, idx: number) => {
       if (player !== 0) return;
       if (id !== players[currPlayer]) return window.alert(`Not your chance`);
-      // if no more clicks, switch the chance
-      if (clicks === 1) {
-        await axios.put(`${REST_API}/games/update_chance`, {gameId}, {
-          headers: {Authorization: `Bearer ${token}`,}
-        });
-        updateChance!();
-        socket?.emit("updateChance", `LOBBY:${gameId}`);
-        return;
-      }
       // else, update the board
       try {
         setLoading!(true);
-        const reqBody = {currTeamId, typ, idx, oppTeamId, initial};
+        const reqBody = { currTeamId, typ, idx, oppTeamId, initial };
         // update the board
-        await axios.put(`${REST_API}/boards/update_board`, {...reqBody}, {
-          headers: {Authorization: `Bearer ${token}`,}
-        });
+        await axios.put(
+          `${REST_API}/boards/update_board`,
+          { ...reqBody },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
         fetchBoard!(currTeamId);
         // if initial update only team
         if (initial) socket?.emit("updateBoard", currTeamId);
@@ -47,8 +42,23 @@ const GameGrid: React.FC<GameGridProps> = () => {
         else socket?.emit("updateBoard", `LOBBY:${gameId}`);
         setClicks!(clicks - 1);
         setLoading!(false);
-        window.alert(`${clicks-1} clicks left`);
-      } catch (err) {setLoading!(false);}
+        window.alert(`${clicks - 1} clicks left`);
+      } catch (err) {
+        setLoading!(false);
+      }
+      // if no more clicks, switch the chance
+      if (clicks === 1) {
+        await axios.put(
+          `${REST_API}/games/update_chance`,
+          { gameId },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        updateChance!();
+        socket?.emit("updateChance", `LOBBY:${gameId}`);
+        return;
+      }
     };
 
     return (
